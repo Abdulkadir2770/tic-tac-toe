c9de00f472c7943178550f3af85353d26cf6b88e
Amadi Y <absnacks17@gmail.com>
2022-08-23 23:48:06 -0500

    added the O and X

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
-const gameBoard = (() => {
+const boxes = Array.from(document.getElementsByClassName("box"));
+const play_text = document.getElementById("text");
+const restart = document.querySelector("button");
 
-    let board = [];
-})();
 
-const createPlayer = (name, marker) => {
-    return {name, marker};
+const spaces = [null, null, null, null, null, null, null, null, null];
+const O_Text = "O";
+const X_Text = "X";
+let current_player = O_Text;
+
+const game_board = () => {
+    boxes.forEach((box) => {
+        box.addEventListener("click", (e) => {
+            const id = e.target.id;
+            console.log(id);
+
+            if(!spaces[id]) {
+                spaces[id] = current_player;
+                e.target.innerText = current_player;
+
+                if(player_has_won) {
+                    play_text.innerText = `${current_player} has won`;
+                    return;
+                }
+            } else if(spaces[id] === O_Text) {
+                console.log
+            }
+        })
+    })
 }
 
-const game = (() => {
+const player_has_won = () => {
+    
+}
 
-})();
+game_board();
+
+// restarts the game when pressed
+restart.addEventListener("click", )
